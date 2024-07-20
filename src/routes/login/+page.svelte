e603eda0f445a6efb39d8a472ae7464c0edef2b4
Daniel Freyr <danielfrs87@gmail.com>
2024-07-20 17:42:10 +0000

    🔧 Github pages test

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,7 @@
 <script>
-	import Login from '../../Components/Login/Login.svelte';
 </script>
 
 <svelte:head>
 	<title>Login</title>
 	<meta name="description" content="Login" />
 </svelte:head>
-
-<Login />
