f4caa6436f459bc4199058a7f92b48a66bf4f9dd
Daniel Freyr <danielfrs87@gmail.com>
2024-07-20 22:09:15 +0000

    🔧 Testing stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 	<Pricing />
 	<!--Footer-->
 	<div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
-		<a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; App 2019</a>
+		<p>danni</p>
 	</div>
 </div>
 
